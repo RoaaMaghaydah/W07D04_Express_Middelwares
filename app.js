6355edc17bae43b331c0a6c76e2255c8047ee629
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-05-12 12:46:32 +0300

    Pluse chack

--- app.js
+++ app.js
@@ -3,20 +3,33 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const users = ["John", "Mark"];
+const users = [];
 
 const logUsers=(req,res,next)=>{
     console.log("function log user")
     res.json(users)       
      next()
 }
+const logMethod =(req,res,next)=>{
+console.log("function logMethod")
+next();
+}
 
 app.use(logUsers);
+app.use("/users",logMethod)
+app.use(express.json())
 
 app.get("/users", (req, res, next) => {
   res.json(users);
 });
 
+app.use((error1,req,res,next)=>{
+    if(users.length===0){ 
+           next("errer");
+           console.log("no users");
+           res.json("no users");
+      }   
+})
 
 
 app.listen(port, () => {
