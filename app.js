056b89fecc71dd210605b463b7b4cfffcb3ac765
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-05-12 11:59:08 +0300

    Pluse chack q1

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const port = 3000;
 
 const users = ["John", "Mark"];
 
+const logUsers=(req,res,next)=>{
+    console.log("function log user")
+    res.json(users)       
+     next()
+}
+
+app.use(logUsers);
+
 app.get("/users", (req, res, next) => {
   res.json(users);
 });
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
